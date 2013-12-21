5b7b721e20b8632db3fe5ba5dde556ca72b49feb
Michael James <michael@michaelbjames.com>
2013-12-21 08:34:54 -0500

    we have some hrefs now

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 var express = require('express'),
           _ = require('underscore'),
-       request = require('request');
+    request = require('request'),
+      jsdom = require('jsdom');
 
 var app = express();
 
 app.use('/solve', function(req, res, next){
-  console.log(req.query);
   if(_.isUndefined(req.query.start)){
     return res.send(400, "Please specify a start address");
   }
@@ -23,12 +23,21 @@ app.use('/solve', function(req, res, next){
 var wikibase = 'http://en.wikipedia.org/wiki/'
 
 app.get('/solve', function(req, res){
-  console.log(wikibase + req.query.start);
-  request(wikibase + req.query.start, function(err, response, body){
-    if (!err && response.statusCode == 200) {
-      console.log(body)
-    }
-  });
+  // request(wikibase + req.query.start, function(err, response, body){
+  //   if (!err && response.statusCode == 200) {
+  //     console.log(body)
+  //   }
+  // });
+
+  jsdom.env(
+    wikibase + req.query.start,
+    ["http://code.jquery.com/jquery.js"],
+    function(err,window){
+      window.$("#mw-content-text a").each(function(i){
+        console.log((window.$("#mw-content-text a")[i]).href);
+      });
+    });
+
   res.send(200);
 });
 
