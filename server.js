35d7ae5496a203dbbc3d59897e522f35983d0299
Michael James <michael@michaelbjames.com>
2013-12-20 21:35:11 -0500

    got some boilerplate setup

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var express = require('express');
+"use strict";
+
+var express = require('express'),
+          _ = require('underscore');
+
 var app = express();
+
+app.use('/solve', function(req, res, next){
+  console.log(req.query);
+  if(_.isUndefined(req.query.start)){
+    return res.send(400, "Please specify a start address");
+  }
+  if(_.isUndefined(req.query.end)){
+    return res.send(400, "Please specify an end");
+  }
+  next();
+});
 
 app.get('/solve', function(req, res){
   res.send(200);
 });
+
+app.listen(8080);
