93e25f6a666cb23fcf8effcb501ce01529a43721
Michael James <michael@michaelbjames.com>
2013-12-31 23:38:34 -0500

    neighbors function written

--- server.rb
+++ server.rb
@@ -1,5 +1,39 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-get '/' do
+WIKIBASE = 'http://en.wikipedia.org/wiki/'
+
+def neighbors(name)
+  article = Nokogiri::HTML(open(WIKIBASE + name))
+  # binding.pry
+  connected = Array.new
+  article.css('#mw-content-text a').each do |link|
+      value = link.attr('href')
+      if value =~ /^\/wiki\/.*$/
+        connected.push(value.gsub('/wiki/',''))
+      end
+  end
+  puts connected.uniq
+  connected.uniq
+end
+
+def dijkstra(start, finish)
+  
+end
+
+get '/solve' do
+  if params[:start].nil?
+    status 400
+    body 'Missing start parameter'
+    return
+  end
+  if params[:end].nil?
+    status 400
+    body 'Missing end parameter'
+    return
+  end
+  neighbors(params[:start])
   'Hello world!'
 end
