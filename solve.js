28a22aa3b7257da4202d817053a0e1480fc7103e
Michael James <michael@michaelbjames.com>
2014-01-02 17:55:56 -0500

    tree structure otw, but it's broken

--- solve.js
+++ solve.js
@@ -1,72 +1,91 @@
 var w = window.innerWidth, h = window.innerHeight;
 
-var color = d3.scale.category10();
+var duration = 750;
 
-var nodes = [],
-    links = [];
+var tree = d3.layout.tree()
+                    .size([w,h]);
 
-var force = d3.layout.force()
-    .nodes(nodes)
-    .links(links)
-    .charge(-400)
-    .linkDistance(120)
-    .size([w, h])
-    .on("tick", tick)
-    .start();
+var root = {},
+   nodes = tree(root);
 
-var svg = d3.select("#d3container").append("svg:svg").attr("width", w).attr("height", h);
+root.parent = root;
+root.px = root.x;
+root.py = root.y;
+
+var diagonal = d3.svg.diagonal();
+
+var svg = d3.select("#d3container").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("g")
+    .attr("transform", "translate(10,10)");
 
 var node = svg.selectAll(".node"),
     link = svg.selectAll(".link");
 
-
-function start() {
-  link = link.data(force.links(), function(d) { return d.source.id + "-" + d.target.id; });
-  link.enter().insert("line", ".node").attr("class", "link");
-  link.exit().remove();
-
-  node = node.data(force.nodes(), function(d) { return d.id;});
-  node.enter().append("circle").attr("class", function(d) { return "node " + d.id; }).attr("r", 8);
-  node.exit().remove();
-
-  force.start();
+function indexOf(list, key, value){
+  for (var i = 0; i < list.length; i++) {
+    if(list[i][key] == value){
+      return i;
+    }
+  }
+  return -1;
 }
-
-function tick() {
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-}
-
-// 1. Add three nodes and three links.
-// setTimeout(function() {
-//   var a = {id: "a"}, b = {id: "b"}, c = {id: "c"};
-//   nodes.push(a, b, c);
-//   links.push({source: a, target: b}, {source: a, target: c}, {source: b, target: c});
-//   start();
-// }, 0);
-
-var reprocess = _.debounce(start,100);
 
 function receive (event) {
   console.log(event.data);
   var msg = JSON.parse(event.data);
   switch(msg.type) {
     case "progress":
-      if(_.isNull(msg.previous)){ //Weird first case
-        nodes.push({id: msg.current});
+      var n = {id: msg.current},
+          p = {id: msg.previous},
+          index = indexOf(nodes, "id", msg.previous),
+          pn = nodes[Math.random() * nodes.length | 0];
+      console.log(index);
+      if(index < 0){
+        p.children = [n];
+        nodes.push(p);
       } else {
-        if(!_.contains(nodes, msg.previous)){
-          nodes.push({id: msg.previous});
-        }
-        nodes.push({id:msg.current});
-        links.push({source: {id: msg.previous}, target: {id:msg.current}});
+        pn = nodes[index];
+        if (pn.children)
+          pn.children.push(n);
+        else
+          pn.children = [n];
       }
-      reprocess();
+    nodes.push(n);
+
+    console.log(nodes);
+
+    // Recompute the layout and data join.
+    node = node.data(tree.nodes(root), function(d) { return d.id; });
+    link = link.data(tree.links(nodes), function(d) { return d.source.id + "-" + d.target.id; });
+
+    // Add entering nodes in the parent’s old position.
+    node.enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 4)
+        .attr("cx", function(d) { return d.parent.px; })
+        .attr("cy", function(d) { return d.parent.py; });
+
+    // Add entering links in the parent’s old position.
+    link.enter().insert("path", ".node")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: d.source.px, y: d.source.py};
+          return diagonal({source: o, target: o});
+        });
+
+    // Transition nodes and links to their new positions.
+    var t = svg.transition()
+               .duration(duration);
+
+    t.selectAll(".link")
+              .attr("d", diagonal);
+
+    t.selectAll(".node")
+              .attr("cx", function(d) { return d.px = d.x; })
+              .attr("cy", function(d) { return d.py = d.y; });
+
       break;
     case "solution":
       break;
